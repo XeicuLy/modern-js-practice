179212fc2dfe19350e3e7ac31cbe12aec71dcc57
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2022-11-24 16:14:26 +0900

    first commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
+
+export default defineConfig({
+  server: {
+    port: 8080,
+    open: '/src/index.html',
+  },
+  plugins: [
+    legacy({
+      targets: ['ie >= 11'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    }),
+  ],
+});
